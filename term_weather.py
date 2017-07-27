a0b357542e3d8a3d1da3f183806d0610ca75565d
KevinMidboe <kevin.midboe@gmail.com>
2017-07-27 21:39:15 +0200

    Starting class with todo list for first development

---
+++ term_weather.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+# @Author: KevinMidboe
+# @Date:   2017-07-27 21:26:53
+# @Last Modified by:   KevinMidboe
+# @Last Modified time: 2017-07-27 21:33:30
+
+# TOD LIST
+# Get coordinates from IP
+# Fetch coordinates from YR
+# Parse return data
+# Match wheater description to icons 
+
+import fire
+
+class termWeather(object):
+	
+	def fetch(self, town):
+		pass
+
+if __name__ == '__main__':
+	fire.Fire(termWeather)
